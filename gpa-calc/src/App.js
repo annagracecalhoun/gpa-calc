1728548bebd1b3bf8acd57a074367496a221c105
annagracecalhoun <54077176+annagracecalhoun@users.noreply.github.com>
2021-11-22 20:43:46 -0500

    Update App.js

--- gpa-calc/src/App.js
+++ gpa-calc/src/App.js
@@ -9,6 +9,8 @@ import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 function App() {
   const [userName, setuserName] = useState(''); 
   const [passWord, setpassWord] = useState(''); 
+  const [valLogin, setvalLogin] = useState(false); 
+  const [logFail, setlogFail] = useState(false); 
 
   useEffect(()=> {
     Axios.get('http://localhost:3001/api/getUser').then((response) => 
@@ -37,10 +39,11 @@ function App() {
     Axios.get('http://localhost:3001/api/getUser').then((response) => {
     const isValid = response.data.find((o) => o.username === userName && o.password === passWord); 
     if (isValid) {
-      console.log("valid login"); 
+      setvalLogin(true); 
     }
     else {
-      console.log("invalid login")
+      setvalLogin(false); 
+      setlogFail(true); 
     }
   }); 
   }
@@ -49,7 +52,9 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
+          
           <Route exact path="/"> 
+          {!valLogin? 
           <div className="logScreen">
             <span>Login to your account</span>
             <div className="form">
@@ -58,11 +63,17 @@ function App() {
             <label>Password</label>
             <input type="text" name="password" onChange={changePw}></input>
             <button onClick={tryLogin}>Login</button>
+            {logFail? <span>Login Failed</span>: null}
             </div>
             <span>Don't have an account?</span>
             <button><Link to="/createNew">Create New Account</Link></button>
-            </div>
-          </Route>
+            </div> :null}
+            {valLogin? <div>
+              <span>You are logged in!</span>
+
+            </div> : null}
+          </Route>: 
+          
           <Route exact path="/createNew">
       <span>Create an Account (adds username and pw to db)</span>
       <div className="form">
@@ -70,7 +81,7 @@ function App() {
       <input type="text" name="username" onChange={changeUse}></input>
       <label>Password</label>
       <input type="text" name="password" onChange={changePw}></input>
-      <button onClick={createAccount}>Submit</button>
+      <button onClick={createAccount}><Link to="/">Submit</Link></button>
       </div>
       </Route>
       </Switch>
@@ -80,4 +91,3 @@ function App() {
 }
 
 export default App;
-

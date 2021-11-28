dae785d6da1704dda2cb15fffde6b88534dd96bb
Shoaib <42903226+shoaibmrana@users.noreply.github.com>
2021-11-28 18:20:26 -0500

    Added Change Password functionality + Updated some design

--- gpa-calc/src/App.js
+++ gpa-calc/src/App.js
@@ -2,21 +2,23 @@
 // https://www.youtube.com/watch?v=_S2GKnFpdtE 
 import './App.css';
 import MainPage from './components/MainPage';
-import {React, useEffect, useState} from "react"; 
-import Axios from 'axios'; 
-import {BrowserRouter as Router, Switch, Route, Link, useHistory} from "react-router-dom"; 
+import { React, useEffect, useState } from "react";
+import Axios from 'axios';
+import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
 import AddCourse from './components/AddCourse';
-
+import ChangePassword from './components/ChangePassword';
 
 function App() {
-  const [userName, setuserName] = useState(''); 
-  const [passWord, setpassWord] = useState(''); 
-  const [valLogin, setvalLogin] = useState(false); 
-  const [logFail, setlogFail] = useState(false); 
-  const [compID, setcompID] = useState(''); 
-  const [maj, setMaj] = useState([]); 
+  const [userName, setuserName] = useState("");
+  const [passWord, setpassWord] = useState("");
+  const [valLogin, setvalLogin] = useState(false);
+  const [logFail, setlogFail] = useState(false);
+  const [compID, setcompID] = useState('');
+  const [maj, setMaj] = useState([]);
 
- 
+  const [user, setUser] = useState();
+
+
   useEffect(() => {
     Axios.get('http://localhost:3001/api/studMajor').then(response => {
       let allMaj = []
@@ -25,126 +27,135 @@ function App() {
           allMaj.push(o.major)
         }
       })
-      setMaj(allMaj); 
+      setMaj(allMaj);
     })
   }, [compID])
 
   const changeUse = (e) => {
-    setuserName(e.target.value); 
+    setuserName(e.target.value);
   }
 
   const changePw = (e) => {
-    setpassWord(e.target.value); 
+    setpassWord(e.target.value);
   }
 
   const createAccount = () => {
-    Axios.post('http://localhost:3001/api/create', {useName: userName, pw: passWord}).then(() => {
-      setuserName(''); 
-      setpassWord(''); 
-    }); 
+    Axios.post('http://localhost:3001/api/create', { useName: userName, pw: passWord }).then(() => {
+      setuserName('');
+      setpassWord('');
+    });
   }
 
   const deleteAccount = () => {
-    Axios.post('http://localhost:3001/api/delete', {useName: userName, pw: passWord}).then(() => {
-      setuserName(''); 
-      setpassWord(''); 
-    }); 
+    Axios.post('http://localhost:3001/api/delete', { useName: userName, pw: passWord }).then(() => {
+      setuserName('');
+      setpassWord('');
+    });
   }
 
-  const changePassword = () => {
-    Axios.post('http://localhost:3001/api/passUpdate', {useName: userName, pw: passWord}).then(() => {
-      setuserName(''); 
-      setpassWord(''); 
-      setvalLogin(true); 
-    }); 
-  }
 
-  const tryLogin = () => {
+  const tryLogin = async () => {
     Axios.get('http://localhost:3001/api/getUser').then((response) => {
-    const isValid = response.data.find((o) => o.username === userName && o.password === passWord); 
-    if (isValid) {
-      const compID2 = userName.split('@')[0];      // get comp id from email
-      setcompID(compID2); 
-      setvalLogin(true); 
-    }
-    else {
-      setvalLogin(false); 
-      setlogFail(true); 
-    }
-  }); 
+      const isValid = response.data.find((o) => o.username === userName && o.password === passWord);
+      if (isValid) {
+        const compID2 = userName.split('@')[0];      // get comp id from email
+        setcompID(compID2);
+
+        localStorage.setItem('username', userName)
+        localStorage.setItem('password', passWord)
+
+        setvalLogin(true);
+      }
+      else {
+        setvalLogin(false);
+        setlogFail(true);
+      }
+    });
   }
+
+  const handleLogout = () => {
+    setUser({});
+    setuserName("");
+
+  }
+
 
   return (
     <div className="App">
       <Router>
         <Switch>
+          {/* Add Course Page */}
           <Route exact path="/addCourse">
-              <AddCourse name={userName} compId={compID}></AddCourse>
-            </Route>
-            <Route exact path ="/delete">
+            <AddCourse name={userName} compId={compID}></AddCourse>
+          </Route>
+
+          {/* Delete Course Page */}
+          <Route exact path="/delete">
             <span>Confirm username and password to delete account</span>
-      <div className="form">
-        <label>Username (uva email address)</label>
-      <input type="text" name="username" onChange={changeUse}></input>
-      <label>Password</label>
-      <input type="text" name="password" onChange={changePw}></input>
-      <button onClick={deleteAccount}><Link to="/">Delete</Link></button>
-      </div>
-            </Route>
-            <Route exact path="/change-password">
-            <span>Type username and new password</span>
-      <div className="form">
-        <label>Username (uva email address)</label>
-      <input type="text" name="username" onChange={changeUse}></input>
-      <label>New Password</label>
-      <input type="text" name="password" onChange={changePw}></input>
-      <button onClick={changePassword}><Link to="/">Update password</Link></button>
-</div>
-            </Route>
-          <Route exact path="/"> 
-          {!valLogin? 
-          <div className="logScreen">
-            <div className="mainHead">
-            <span>Log in to your UVA GPA Calculator account</span>
-            </div>
             <div className="form">
-            <label>Username (must be your UVA email address)</label>
-            <input type="text" name="username" onChange={changeUse}></input>
-            <label>Password</label>
-            <input type="text" name="password" onChange={changePw}></input>
-            <button  className="logButton" onClick={tryLogin}>Login</button>
-            {logFail? <span>Login Failed</span>: null}
+              <label>Username (uva email address)</label>
+              <input type="text" name="username"></input>
+              <label>Password</label>
+              <input type="text" name="password"></input>
+              <button onClick={deleteAccount}><Link to="/">Delete</Link></button>
             </div>
-            <div className="newAcc">
-            <span className="noAcc">Don't have an account?</span>
-            <button className="logButton"><Link to="/createNew">Create New Account</Link></button>
-            </div>
-            </div> :null}
-            {valLogin? 
-             <div>
-              <MainPage name={userName} compId={compID}></MainPage>
-            </div> : null}
-          </Route>: 
+          </Route>
+
+          {/* Change Password Page */}
+          <Route exact path="/change-password">
+            <ChangePassword></ChangePassword>
+          </Route>
+
+
+          {/* Home Page */}
+          <Route exact path="/">
+            {!valLogin ?
+              <div className="logScreen">
+                <div className="mainHead">
+                  <span>Log in to your UVA GPA Calculator account</span>
+                </div>
+                <div className="form">
+                  <label>Username (must be your UVA email address)</label>
+                  <input type="text" name="username" onChange={changeUse}></input>
+                  <label>Password</label>
+                  <input type="text" name="password" onChange={changePw}></input>
+                  <button className="logButton" onClick={tryLogin}>Login</button>
+                  {logFail ? <span>Login Failed</span> : null}
+                </div>
+                <div className="newAcc">
+                  <span className="noAcc">Don't have an account?</span>
+                  <button className="logButton"><Link to="/createNew">Create New Account</Link></button>
+                </div>
+              </div> : null}
+
+            {valLogin ?
+              <div>
+                <MainPage name={userName} compId={compID}></MainPage>
+              </div> : null}
+          </Route>
+
+
+          {/* Create New Account Page */}
           <Route exact path="/createNew">
-      <span>Create an Account</span>
-      <div className="form">
-        <label>Username (uva email address)</label>
-      <input type="text" name="username" onChange={changeUse}></input>
-      <label>Password</label>
-      <input type="text" name="password" onChange={changePw}></input>
-      <button onClick={createAccount}><Link to="/">Create</Link></button>
-      </div>
-      </Route>
-      <Route exact path="/details">
-            <div className="accInfo">
-            <span><strong>Account Info</strong></span>
-            <span><strong>Username: </strong>{userName}</span>
-            <span><strong>Computing id: </strong>{compID}</span>
-            <span><strong>Majors: </strong><ul>
-              {maj.map((x) => <li>x</li>)}</ul></span>
+            <span>Create an Account</span>
+            <div className="form">
+              <label>Username (uva email address)</label>
+              <input type="text" name="username" onChange={changeUse}></input>
+              <label>Password</label>
+              <input type="text" name="password" onChange={changePw}></input>
+              <button onClick={createAccount}><Link to="/">Create</Link></button>
             </div>
-        </Route>
-      </Switch>
+          </Route>
+          <Route exact path="/details">
+            <div className="accInfo">
+              <span><strong>Account Info</strong></span>
+              <span><strong>Username: </strong>{userName}</span>
+              <span><strong>Computing id: </strong>{compID}</span>
+              <span><strong>Majors: </strong><ul>
+                {maj.map((x) => <li>x</li>)}</ul></span>
+            </div>
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
---
+++ gpa-calc/src/components/ChangePassword.js
@@ -0,0 +1,78 @@
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
+import Axios from 'axios';
+
+function ChangePassword() {
+
+
+    const [currentPassword, setCurrentPassword] = useState(localStorage.password);
+    const [newPassword, setNewPassword] = useState();
+    const [currentPassCheck, setCurrentPassCheck] = useState(false);
+    const [passwordChanged, setPasswordChanged] = useState(false);
+    const [changeFail, setChangeFail] = useState(false);
+
+
+    const checkCurrentPass = (e) => {
+        Axios.get('http://localhost:3001/api/getUser').then((response) => {
+            const isValid = response.data.find((o) => o.username === localStorage.username && o.password === e.target.value);
+
+            //  console.log("Checking user: ", localStorage.username, " with password: ", e.target.value);
+
+            if (isValid) { // match found in database => valid current password provided
+                console.log("success");
+                setCurrentPassCheck(true);
+            }
+            else {
+                console.log("fail");
+                setCurrentPassCheck(false);
+            }
+        });
+    }
+
+    const getNewPass = (e) => {
+        setNewPassword(e.target.value);
+    }
+
+    const changePassword = () => {
+        if (currentPassCheck === true && newPassword) {
+            console.log(newPassword)
+            Axios.post('http://localhost:3001/api/passUpdate', { useName: localStorage.username, pw: newPassword }).then(() => {
+                localStorage.setItem('password', newPassword);
+            });
+            setPasswordChanged(true);
+        } else {
+            setChangeFail(true);
+        }
+    }
+
+    return (
+        <div>
+            {passwordChanged ?
+                <div>
+                    <h1>Success!</h1>
+                    <h5>Your password has been successfully updated.</h5>
+                    <button style={{ marginTop: "10px" }}><Link to="/">Login</Link></button>
+                </div>
+                : null}
+
+            {!passwordChanged ?
+                <div>
+                    <span>Type current password and new password</span>
+
+                    <div className="form">
+                        <label>Current Password</label>
+                        <input type="text" name="currentPass" onChange={checkCurrentPass}></input>
+                        <label>New Password</label>
+                        <input type="text" name="newPassword" onChange={getNewPass}></input>
+                        <button onClick={changePassword}>Update password</button>
+                        {changeFail ? <span>Error.</span> : null}
+                    </div>
+
+                    <button style={{ marginTop: "10px" }}><Link to="/">Cancel</Link></button>
+                </div>
+                : null}
+        </div>
+    )
+};
+
+export default ChangePassword;

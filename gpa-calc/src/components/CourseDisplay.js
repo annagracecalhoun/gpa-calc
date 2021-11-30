b303d1bfed78253be93d5357e521d687b39c6161
Carter Bristow <cb6xj@virginia.edu>
2021-11-30 13:03:18 -0500

    edit course

--- gpa-calc/src/components/CourseDisplay.js
+++ gpa-calc/src/components/CourseDisplay.js
@@ -19,7 +19,7 @@ function CourseDisplay (props) {
 
     const delCourse = () => {
         // need to update the interface
-        Axios.post('http://localhost:3001/api/delCourse', {compID: props.compID, subject: props.courseSub, courseNum: props.courseNum, grade: props.courseGrade})
+        Axios.post('http://localhost:3001/api/delCourse', {compID: props.compID, subject: props.courseSub, courseNum: props.courseNum, grade: cGrade})
         setdelCourse2(true); 
         // need to re-update courses
     }
